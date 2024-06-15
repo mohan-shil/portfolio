f14d69767d56b25d621201689134ff9276ac25a4
mohan shil <mohanshil05@gmail.com>
2024-06-15 17:34:16 +0200

    added experience and approach section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 
+import { navItems } from "../../data";
+import Approach from "./components/Approach";
+import Experience from "./components/Experience";
 import Hero from "./components/Hero";
 import RecentProjects from "./components/RecentProjects";
 import { FloatingNav } from "./components/ui/FloatingNav";
@@ -9,12 +12,12 @@ export default function Home() {
   return (
    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5  ">
     <div className="max-w-7xl w-full">
-      <FloatingNav navItems={[
-        {name: 'Home', link: '/', }
-      ]} />
+      <FloatingNav navItems={ navItems } />
       <Hero />
       <Grid />
       < RecentProjects/> 
+      <Experience />
+      <Approach />
       </div>
     
    </main>
